716e7300c7e60038b21cc8194e3458f3741e6f31
rifkydarmawan62 <rifkydarmawan62@gmail.com>
2024-03-21 19:22:15 +0700

    Optimalisasi Program

--- Windows_Subsystem_Linux/Aktifkan_Fitur_WSL.cmd
+++ Windows_Subsystem_Linux/Aktifkan_Fitur_WSL.cmd
@@ -1,4 +1,6 @@
 @title Aktifkan Fitur Windows Subsystem Linux ^(WSL^)
-dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all
+@echo Memeriksa hak administrator ...
+@"%windir%\system32\cacls.exe" "%windir%\system32\config\system"
+@if %errorlevel% EQU 0 ( dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all ) else ( echo Akses administrator diperlukan untuk menjalankan program )
 @pause
