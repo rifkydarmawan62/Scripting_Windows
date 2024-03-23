443117c107c031f2e7b12ef2e68468684fabbdda
rifkydarmawan62 <rifkydarmawan62@gmail.com>
2024-03-23 13:18:12 +0700

    File diperbarui

--- Online/Download/Download_Windows_10.cmd
+++ Online/Download/Download_Windows_10.cmd
@@ -1,2 +1,2 @@
-@title Download Windows 10 ^(online^)
+@title https://go.microsoft.com/fwlink/?LinkId=691209
 @start "Download Windows 10 (online)" /MAX https://go.microsoft.com/fwlink/?LinkId=691209
