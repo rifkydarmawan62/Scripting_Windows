976d5f9f29860801bf2ad5eb6afc188be5f8ad22
rifkydarmawan62 <rifkydarmawan62@gmail.com>
2024-04-25 19:56:58 +0700

    Fitur IIS ditambahkan

---
+++ Internet_Information_Services/IIS_6.0_Manager.cmd
@@ -0,0 +1,2 @@
+@title C:\WINDOWS\system32\inetsrv\InetMgr6.exe
+@start "C:\WINDOWS\system32\inetsrv\InetMgr6.exe" /MAX C:\WINDOWS\system32\inetsrv\InetMgr6.exe
---
+++ Internet_Information_Services/IIS_Manager.cmd
@@ -0,0 +1,2 @@
+@title C:\WINDOWS\system32\inetsrv\InetMgr.exe
+@start "Internet Information Services (IIS) Manager" /MAX C:\WINDOWS\system32\inetsrv\InetMgr.exe
